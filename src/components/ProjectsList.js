7958c4a51246bf46bbfa555edce40bba04d1009a
hejeong <jonhong777@gmail.com>
2019-07-18 10:25:42 -0400

    Add second project to fake data

--- src/components/ProjectsList.js
+++ src/components/ProjectsList.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import SinatraTextbooks from '../assets/sinatra-college-textbooks.png';
+import RunnersWeekly from '../assets/runners-weekly.png';
+
 import ProjectCard from './ProjectCard.js';
 const ProjectsList = () => {
     return <div>
-       <ProjectCard />
+       <div className="card">
+        <img src={ SinatraTextbooks } alt="college-textbooks-sinatra" className="img"/>
+        <div className="tags">
+            <p className="ruby-tag tag">Ruby Sinatra</p>
+        </div>
+    </div>
+    <div className="card">
+        <img src={ RunnersWeekly } alt="runners-weekly" className="img"/>
+        <div className="tags">
+            <p className="ruby-tag tag">Ruby on Rails</p>
+            <p className="javascript-tag tag">React/Redux</p>
+        </div>
+    </div>
     </div>
 }
 
