1b13dbe392a14c62d5dffb14c3d210ce26a77a9e
hejeong <jonhong777@gmail.com>
2019-08-12 19:22:10 -0400

    Add static web development projects in the projects section on home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import SinatraTextbooks from '../assets/sinatra-college-textbooks.png';
+import RunnersWeekly from '../assets/runners-weekly.png';
 const Home = () => {
     return  <div>
         <section id="home-header">
@@ -8,14 +10,31 @@ const Home = () => {
                 <div className="links-container">
                 <a href="/projects" >Projects</a>
                 <p className="text-divider">  |  </p>
-                <a href="#">Blog </a>  
-                <p className="text-divider">  |  </p>
+                <a href="#">Blog</a>  
+                <p className="text-divider">   |  </p>
                 <a href="#">Contact</a>
                 </div>
             </div>
         </section>
         <section id="home-projects">
-            <h1>Projects</h1>
+            <div id="top-projects">
+                <h1>Web Development Projects</h1>
+                <div className="project clearfix"> 
+                    <div className="project-text float-left left-item">
+                        <p className="project-title">Sinatra Textbooks</p>
+                        <p className="project-desc">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                    </div>
+                    <img src={ SinatraTextbooks } alt="college-textbooks-sinatra" className="medium-img float-left right-item"/>
+                </div>
+                <div className="project clearfix"> 
+                    <img src={ RunnersWeekly } alt="college-textbooks-sinatra" className="medium-img float-left left-item"/>
+                    <div className="project-text float-left right-item">
+                        <p className="project-title">RunnersWeekly</p>
+                        <p className="project-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse mollis sagittis ipsum tincidunt porttitor. Quisque iaculis in mauris quis eleifend. Cras in consequat mi. Etiam maximus libero eu vulputate malesuada. In et magna et mauris vestibulum dictum efficitur ac orci. Quisque congue auctor mi, placerat blandit neque vestibulum a. Pellentesque eleifend orci sit amet odio venenatis sollicitudin a vitae ipsum. Phasellus venenatis arcu sed ante sollicitudin, et varius sem consectetur. Nulla eget ornare enim. Duis in condimentum nibh.</p>
+                    </div>
+                </div>
+               
+            </div>
         </section>
   </div>
 }
