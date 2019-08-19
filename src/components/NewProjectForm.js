7be8a869dcd54ab521098fe4e11971371cde9963
hejeong <jonhong777@gmail.com>
2019-08-19 14:09:03 -0400

    Add new project form with onChange and onSubmit actions that send the form data to api

--- src/components/NewProjectForm.js
+++ src/components/NewProjectForm.js
@@ -1,18 +1,47 @@
 import React, {Component} from 'react';
 
 class NewProjectForm extends Component {
+    constructor(){
+        super()
+        this.state={
+            title: "",
+            description: "",
+            content: "",
+            cover_image: ""
+        }
+    }
+    handleOnSubmit = (event) => {
+        event.preventDefault();
+        fetch(`http://localhost:8000/api/projects/`, {
+            method: 'POST',
+            headers: {
+                "Content-Type": 'application/json'
+            },
+            body: JSON.stringify({
+                project: {...this.state}
+            })
+        })
+    }
 
+    handleOnChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
     render(){
         return(<div>
-            <form>
+            <form onSubmit={this.handleOnSubmit}>
                 <h4>Add a new project</h4>
                 <label>Title: </label>
-                <input type="text" />
+                <input type="text" name="title" onChange={this.handleOnChange}/>
                 <label>Description: </label>
-                <input type="text" />
+                <input type="text" name="description" onChange={this.handleOnChange}/>
                 <label>Content: </label>
-                <input type="textarea" />
+                <input type="textarea" name="content" onChange={this.handleOnChange}/>
+                <label>Thumbnail: </label>
+                <input type="file" name="image" onChange={this.handleOnChange}/>
                 <input type="submit" value="submit" />
+                
             </form>
         </div>)
     }
