f990c5feecd0fc09bc9f065e34633b97285f2187
hejeong <jonhong777@gmail.com>
2019-08-28 00:27:36 -0400

    Add styling and wrapper elements in ProjectsContainer

--- src/containers/ProjectsContainer.js
+++ src/containers/ProjectsContainer.js
@@ -3,10 +3,22 @@ import ProjectsList from '../components/ProjectsList';
 import {connect} from 'react-redux';
 const ProjectsContainer = ({projects}) => {
     return<div id="projects-container">
-            <a href="/projects/new">New Project</a>
-            <ul className="project-list">
-                <ProjectsList projects={projects} />
-            </ul>
+            <div id="navbar" className="thin-roboto">
+                <h1>Jonathan Hong</h1>
+                <a href='/'>Home</a>
+                <a href="/projects">Projects</a>
+                <a href="/blogs">Blogs</a>
+                <a href="/about">About</a>
+                <a href="/projects/new" className="new-project-button">+ New Project</a>
+            </div>
+           
+           <div id="project-list-container">
+               <div className="project-list-content">
+                <ul className="project-list">
+                        <ProjectsList projects={projects} />
+                    </ul>
+               </div>
+           </div>
         </div>
 }
 
