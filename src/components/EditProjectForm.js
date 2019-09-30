5f98727656b793d1b076bddc4012f59cb53c7ad9
hejeong <jonhong777@gmail.com>
2019-09-30 14:23:31 -0400

    Add edit to project

--- src/components/EditProjectForm.js
+++ src/components/EditProjectForm.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import { connect } from 'react-redux';
-import { updateProjectForm, createProject } from '../actions/project.js';
+import { updateProjectForm, submitProjectEdit, deleteProject } from '../actions/project.js';
 import { checkToken } from '../actions/adminLogin';
 import { Redirect } from 'react-router';
 
